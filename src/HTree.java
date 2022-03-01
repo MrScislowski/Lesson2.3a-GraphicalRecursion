c9d753e0fc980612e308ac8a878c636738041345
Daniel Scislowski <mr.scislowski@gmail.com>
2022-03-01 11:03:41 -0600

    templated source files; updated .gitignore

--- src/HTree.java
+++ src/HTree.java
@@ -1,23 +1,9 @@
 public class HTree {
     public static void draw(int n, double length, double x, double y) {
-        if (n == 0)
-            return;
 
-        StdDraw.line(x-length/2, y, x+length/2, y);
-        StdDraw.line(x+length/2, y-length/2, x+length/2, y+length/2);
-        StdDraw.line(x-length/2, y-length/2, x-length/2, y+length/2);
-        draw(n-1, length/2, x-length/2, y+length/2);
-        draw(n-1, length/2, x+length/2, y+length/2);
-        draw(n-1, length/2, x-length/2, y-length/2);
-        draw(n-1, length/2, x+length/2, y-length/2);
-    }
-
-    public static void drawHs(int total) {
-        draw(total, 0.5, 0.5, 0.5);
     }
 
     public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);
-        drawHs(n);
+
     }
 }
